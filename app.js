77b2ded72e8f502f6ddd1e85a1dbba9ff80cd2d2
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2023-12-24 11:15:30 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,34 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const User = require("./models/user.js");
 
 mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
 
 db.on("error", (e) => console.log(e));
 db.once("open", () => console.log("connected to DB"));
-app.use(express.json());
 
-app.get("/", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-app.post("/", async (req, res) => {
-  try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    const result = await user.save();
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(500).json({ messge: err.message });
-  }
-});
+const loginRoute = require("./routes/login");
+const signupRoute = require("./routes/signup");
+app.use("/login", loginRoute);
+app.use("/signup", signupRoute);
 app.listen(3000);
